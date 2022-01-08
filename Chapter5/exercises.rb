abe4ba1fb564aa5fc7f1adf143aa0f49f997f3fb
Pete Smith <peadar81@gmail.com>
2022-01-08 09:39:38 +0000

    Update Heroku app

---
+++ Chapter5/exercises.rb
@@ -0,0 +1,8 @@
+# Exercises 5.1.1
+# 1
+def square(value)
+    value**2
+end
+
+puts "Exercise 5.1.1 - 1"
+puts "The square of 5 is #{square(5)}"
--- hello_app.rb
+++ hello_app.rb
@@ -1,7 +1,5 @@
 require 'sinatra'
-require 'date'
 
 get '/' do
-    dayname = Date::DAYNAMES[Time.now.wday]
-    "Hello, world! Happy #{dayname}"
+    "Hello, world! Happy #{day_of_the_week(Time.now)}"
 end
